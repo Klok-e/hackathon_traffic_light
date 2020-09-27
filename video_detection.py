4e946614ac38d31ed319c98334f72024a084b8ed
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2020-09-27 21:49:09 +0300

    remove comments

--- video_detection.py
+++ video_detection.py
@@ -28,43 +28,6 @@ def capture_images_continually(capture: cv2.VideoCapture, model, classes, img_si
             cv2.putText(frame, label[0], (x0, y0 + 30), font, 2, (0, 255, 0), 2)
             cv2.putText(frame, f"{box.confidence:.2}", (x0 + 50, y0 + 30), font, 2, (0, 255, 0), 2)
 
-        # # Detecting objects
-        # blob = cv2.dnn.blobFromImage(frame, 1. / 255., (416, 416), (0, 0, 0), True, crop=False)
-        # network.setInput(blob)
-        # outs = network.forward(unconnected)
-        #
-        # # Showing informations on the screen
-        # class_ids = []
-        # confidences = []
-        # boxes = []
-        # for out in outs:
-        #     for detection in out:
-        #         scores = detection[5:]
-        #         class_id = np.argmax(scores)
-        #         confidence = scores[class_id]
-        #         if (confidence > 0.5) and (class_id == 2 or class_id == 3 or class_id == 5 or class_id == 7):
-        #             # Object detected
-        #             center_x = int(detection[0] * width)
-        #             center_y = int(detection[1] * height)
-        #             w = int(detection[2] * width)
-        #             h = int(detection[3] * height)
-        #             # Rectangle coordinates
-        #             x = int(center_x - w / 2)
-        #             y = int(center_y - h / 2)
-        #             boxes.append([x, y, w, h])
-        #             confidences.append(float(confidence))
-        #             class_ids.append(class_id)
-        #
-        # indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-        #
-        # font = cv2.FONT_HERSHEY_PLAIN
-        # for i in range(len(boxes)):
-        #     if i in indexes:
-        #         x, y, w, h = boxes[i]
-        #         label = str(classes[class_ids[i]])
-        #         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 215, 0), 2)
-        #         cv2.putText(frame, label, (x, y + 30), font, 3, (0, 255, 0), 3)
-
         if not ret:
             print('no video')
             capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
