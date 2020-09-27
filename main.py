82f4113528b4b1f5d76e91d8df41598b29f0f686
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2020-09-27 12:55:02 +0300

    basic server

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import cv2 as cv
 from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates')
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return flask.redirect("/index.html")
+
 
 def main():
     app.run()
