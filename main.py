39615d48a59b2cf80195e003b6fefbbd191f2a35
unknown <vanyushan@mail.ua>
2020-10-03 23:43:54 +0300

    test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 import flask
 import video_detection
 
@@ -10,7 +11,7 @@ app = Flask(__name__,
 
 @app.route('/')
 def root_redirect():
-    return flask.redirect("/index.html")
+	return flask.redirect("/index.html")
 
 
 @app.route('/index.html')
@@ -23,6 +24,20 @@ def video_feed():
     return flask.Response(video_detection.generate_image_binary(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
+@app.route("/coordinates", methods=['POST'])
+def coordinates():
+	x = request.form['x']
+	print("x=" + x)
+	y = request.form['y']
+	print("y=" + y)
+	return flask.render_template("index.html")
+
+@app.route("/rtl", methods=['POST'])
+def rtl():
+	pass
+	return flask.render_template("index.html")
+
+
 def main():
     import threading
     import cv2
@@ -30,11 +45,11 @@ def main():
     from yolo_model import detect
 
     np.random.seed(42)
-    capture = cv2.VideoCapture("out.mp4")
+    capture = cv2.VideoCapture("C:/Python/Test_test/project/hackathon_traffic_light/out32.mp4")
     img_size = 512
-    model, device = detect.create_model("yolo_model/cfg/yolov3-spp.cfg", "yolo/yolov3-spp-ultralytics.pt", img_size,
+    model, device = detect.create_model("C:/Python/Test_test/project/hackathon_traffic_light/yolo_model/cfg/yolov3-spp.cfg", "C:/Python/Test_test/project/hackathon_traffic_light/yolo/yolov3-spp-ultralytics.pt", img_size,
                                         device="")
-    classes = detect.load_classes("yolo_model/data/coco.names")
+    classes = detect.load_classes("C:/Python/Test_test/project/hackathon_traffic_light/yolo_model/data/coco.names")
 
     t = threading.Thread(target=video_detection.capture_images_continually,
                          kwargs={"capture": capture, "model": model, "classes": classes,
